0f7f754d48d187f4c42b6ebbf01c65d40a9cf144
Nick Dodson <silentcicero@outlook.com>
2015-10-18 12:14:19 -0400

    Update package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'silentcicero:saveas',
   version: '0.0.1',
-  summary: 'Provides a global save file function (saveAs) for Meteor apps.',
+  summary: 'Provides a browser-based save file function (saveAs) for Meteor apps.',
   git: 'https://github.com/SilentCicero/meteor-package-solc',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
